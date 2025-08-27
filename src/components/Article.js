988424bc0fd433bd1d70e8cc216a58651c5d512d
srmacpherson <srmacpherson96@gmail.com>
2025-08-27 10:47:53 +0100

    link to author and css spacing improvements

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,16 +4,24 @@ import Thumbnail from './Thumbnail';
 function Article(props) {
     return (
         <article>
-            <a href={ "https://reddit.com" + props.article.permalink } target="_blank" rel="noreferrer">
-                <h3>{props.article.title}</h3>
-            
-            <div className="article-img">
-                <Thumbnail article={props.article} />
-            </div>
+            <a 
+                href={ "https://reddit.com" + props.article.permalink } target="_blank" rel="noreferrer">
+                    <h3>{props.article.title}</h3>
+                
+                <div className="article-img">
+                    <Thumbnail article={props.article} />
+                </div>
             </a>
-            <p>Author: {props.article.author}</p>
-            <p>Score: {props.article.score}</p>
-            <p>Comments: {props.article.num_comments}</p>
+            <p class='preview'>
+                {props.article.selftext ? props.article.selftext.split(' ').slice(0, 10).join(' ') + '...' : ''}
+            </p>
+            <div class='extras'>
+                <p>
+                    Author: <a href={`https://www.reddit.com/user/${props.article.author}`}>{props.article.author}</a>
+                </p>
+                <p>Score: {props.article.score}</p>
+                <p>Comments: {props.article.num_comments}</p>
+            </div>
         </article>
     )
 }
